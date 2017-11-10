edd9da94c8ddb5a019466a8551f734a9aa52e6ad
Haithem Souala <soualahaitem@gmail.com>
2017-11-10 15:01:38 +0100

    Add files via upload

---
+++ src/main/java/fr/gfi/ldappolicy/AppLogger.java
@@ -0,0 +1,26 @@
+package fr.gfi.ldappolicy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Application logging utility.
+ * 
+ * @author hsouala
+ */
+public class AppLogger {
+
+    private Logger logger;
+    
+    private AppLogger(Class clazz) {
+        logger = LoggerFactory.getLogger(clazz);
+    }
+    
+    public void logException(Exception ex) {
+        logger.error(ex.getLocalizedMessage(), ex);
+    }
+    
+    public static AppLogger getInstance(Class clazz) {
+        return new AppLogger(clazz);
+    }
+}
